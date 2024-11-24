4809c95b79cc46b08738ed776c0ac7ed17d42beb
Anushka6060 <kushananushka12345@gmail.com>
2024-11-24 18:54:36 +0530

    add AOS and ui changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-
+import AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +9,14 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
   templateUrl: './app.component.html',
 
 })
-export class AppComponent { }
+export class AppComponent {
+
+  ngOnInit(): void {
+    AOS.init({
+      duration: 1000, // Animation duration (optional)
+      easing: 'ease-in-out', // Easing type (optional)
+      once: true, // Animation only once (optional)
+      mirror: false // No repeat on scroll up (optional)
+    });
+  }
+}
