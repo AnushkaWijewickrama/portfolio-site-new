395d617add47605670454d4ddc0fc8fa7f4ac94c
Anushka6060 <kushananushka12345@gmail.com>
2024-12-02 10:07:43 +0530

    remove main component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
-import { MainComponent } from './main/main.component';
+import { AppComponent } from './app.component';
+
 
 
 export const routes: Routes = [
-    { path: '', component: MainComponent }
+    { path: '', component: AppComponent }
 ];
